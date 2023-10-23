33ec9af9af45fd526736792abf33305f1cd16632
EduardRo <eduardstancu@yahoo.com>
2023-10-23 12:17:46 +0300

    user table modif

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use DB;
+use Illuminate\Support\Facades\DB;
 
 class UserSeeder extends Seeder
 {
@@ -14,12 +14,39 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            'name'=>'AdminUser',
-            'first_name'=>'Adminescu',
-            'last_name'=>'Adminache',
-            'email'=>'adminuser@investova.ro',
-            
-            
-        ])
+            [
+                'name' => 'Admin',
+                'first_name' => 'Adminescu',
+                'last_name' => 'Adminache',
+                'email' => 'admin@investova.ro',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('parola'),
+
+
+            ],
+            [
+                'name' => 'AminaPavovlici',
+                'first_name' => 'Amina',
+                'last_name' => 'Pavlovici',
+                'email' => 'amina.pavlovici@fondullibertatea.ro',
+                'role' => 'institutional',
+                'status' => 'active',
+                'password' => bcrypt('parola'),
+
+
+            ],
+            [
+                'name' => 'LicaPaulica',
+                'first_name' => 'Paulica',
+                'last_name' => 'Lica',
+                'email' => 'lica.paulica@gmail.ro',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('parola'),
+
+
+            ]
+        ]);
     }
 }
